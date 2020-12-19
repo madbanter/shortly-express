210f4939067cd812b0ee67008479122cc2fcd82a
Adam Brent <adam.brent@gmail.com>
2020-12-18 16:18:08 -0800

    Adds createUser method. Fixes linting issues.

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,9 +2,25 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  //POST
 };
 
 /************************************************************/
 // Add additional authentication middleware functions below
 /************************************************************/
 
+
+//createUser - models.user, on Success use createSession (GET)
+module.exports.createUser = (req, res, callback) => {
+  models.Users.get({username: req.body.username}).then(user => {
+    if (user) {
+      callback('user exists');
+    } else {
+      models.Users.create({username: req.body.username, password: req.body.password});
+    }
+  });
+};
+//login - models.user, on Success use createSession (GET)
+module.exports.login = (req, res, next) => {
+  //a bunch of stuff happens here
+};
